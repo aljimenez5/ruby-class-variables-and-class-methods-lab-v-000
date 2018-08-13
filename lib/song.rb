ea68818c6de874de79eb6ef8a83e3f3b8a5ad8d9
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-13 01:20:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
 
   def self.genre_count
     @genre_count = Hash.new
-    @@genres.each {|genre| @genre_count[genre.uniq, @@genres.count(genre]}
+    @@genres.each {|genre| @genre_count[genre.uniq, @@genres.count(genre)]}
 
   end
 
